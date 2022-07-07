5fc7982e9d629363a2670fa0c8b421f3ff42d029
Yeongkyo In <yeongkyoin@Yeongkyoui-MacBookAir.local>
2022-07-07 14:46:43 +0900

    7월 7일

--- homework/day02/date.js
+++ homework/day02/date.js
@@ -3,6 +3,10 @@ const year = today.getFullYear();
 const month = today.getMonth() + 1;
 const day = today.getDate();
 const hour = today.getHours();
-const 
+const minute = today.getMinutes();
+const second = today.getSeconds();
 
-console.log(`오늘은 ${year}년 ${String(month).padStart(2, "0")}월 ${String(day).padStart(2, "0")}일 ${time}입니다.`);
+console.log(`오늘은 ${year}년 ${String(month).padStart(2, "0")}월 ${String(day).padStart(2, "0")}일 ${String(hour).padStart(2, "0")}:${String(
+    minute
+  ).padStart(2, "0")}:${String(second).padStart(2, "0")}입니다.`
+);
---
+++ homework/day02/template.js
@@ -0,0 +1,23 @@
+function printTemplate({ name, email, resident, phone, site }) {
+  const mytemplate = `
+        <html>
+          <body>
+            <h1>${name}님 가입을 환영합니다.</h1>
+            <hr>
+            <div>이메일: ${email}</div>
+            <div>주민번호: ${resident}</div>
+            <div>휴대폰 번호: ${phone}</div>
+            <div>내가 좋아하는 사이트 : ${site}</div>
+        </body>
+      </html>
+    `;
+  return mytemplate;
+}
+
+const name = "yeongkyo";
+const email = "yeongkyo1997@gmail.com";
+const resident = "970606-1******";
+const phone = "010-8526-1375";
+const site = "google.com";
+
+console.log(printTemplate({ name, email, resident, phone, site }));
