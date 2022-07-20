25cc32222a810a9f982d19cf0ff4e4bbfe1bca03
yeongkyo1997 <yeongkyo1997@gmail.com>
2022-07-20 16:32:31 +0900

    EDIT: day12

--- algorithm/day12/01.js
+++ algorithm/day12/01.js
@@ -1,15 +1,14 @@
+// 콜라츠 추측
 function solution(num) {
-  var answer = 0;
-  while (num !== 1) {
-    if (num % 2 === 0) {
-      num /= 2;
-      answer++;
-    } else {
-      num *= 3;
-      num++;
-      answer++;
-    }
-    if (answer > 500) return -1;
+  let answer = 0
+
+  while (num > 1) {
+    // num이 500보다 크다면 -1을 반환
+    if (num > 500) return -1
+    if (num % 2 === 0) num = num / 2
+    else num = num * 3 + 1
+
+    answer++
   }
-  return answer;
+  return answer
 }
--- algorithm/day12/02.js
+++ algorithm/day12/02.js
@@ -1,12 +1,30 @@
 function solution(numbers) {
-  let temp = [];
+  let temp = []
   for (let i = 0; i < numbers.length; i++) {
     for (let j = 0; j < numbers.length && i !== j; j++) {
-      temp.push(numbers[i] + numbers[j]);
+      temp.push(numbers[i] + numbers[j])
     }
   }
-  let result = [...new Set(temp)];
+  let result = [...new Set(temp)]
   return result.sort((a, b) => {
-    return a - b;
-  });
+    return a - b
+  })
+}
+
+function solution1(numbers) {
+  let temp = []
+  // numbers에서 2개를 뽑아서 더한다.
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length && i !== j; j++) {
+      temp.push(numbers[i] + numbers[j])
+    }
+  }
+
+  // 중복을 제거한다.
+  let result = [...new Set(temp)]
+
+  // 오름차순으로 정렬한다.
+  return result.sort((a, b) => {
+    return a - b
+  })
 }
