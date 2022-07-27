68f20fdac807f81fa154bd4ce6d9f8adb78c881a
yeongkyo1997 <yeongkyo1997@gmail.com>
2022-07-27 10:34:08 +0900

    EDIT: day18 algorithm

--- algorithm/day18/01.js
+++ algorithm/day18/01.js
@@ -1,7 +1,7 @@
 function solution(participant, completion) {
   participant = participant.sort();
   completion = completion.sort();
-
+  
   for (let a = 0; a < participant.length; a++) {
     if (participant[a] !== completion[a]) {
       return participant[a];
